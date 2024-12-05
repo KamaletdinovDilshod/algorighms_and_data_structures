adbc00bf41a53d6b02f01e6d5ec934e9e7e57fc5
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2024-12-05 22:02:03 +0500

    remove unnecessary packages

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.scala
+++
@@ -1,8 +0,0 @@
-import scala.annotation
-
-object Solution {
-  def arrayRankTransform(arr: Array[Int]): Array[Int] = {
-      val rankMap = arr.distinct.sorted.zipWithIndex.toMap
-      arr.map(rankMap).map(_ + 1)
-  }
-}
