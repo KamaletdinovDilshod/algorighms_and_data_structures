15a4c0106fa4a2d79183014853df4ed90e22fd56
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-09-15 20:56:50 +0500

    GreatestCommonDivisorOfStrings

---
+++ src/leetcode/easy/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+public class GreatestCommonDivisorOfStrings {
+    public static void main(String[] args) {
+        String a = "TAUXXTAUXXTAUXXTAUXXTAUXX";
+        String c = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+
+        System.out.println("THIS IS LLLELE: " + 10 % 3);
+//        String subbed = a.substring();
+
+        System.out.println(gcdOfStrings(a, c));
+    }
+
+    public static String gcdOfStrings(String str1, String str2) {
+
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+
+
+        int n = gcd(str1.length(), str2.length());
+        return str1.substring(0, n);
+    }
+
+    public static int gcd(int len1, int len2) {
+        return len2 == 0 ? len1 : gcd(len1, len2);
+    }
+
+}
