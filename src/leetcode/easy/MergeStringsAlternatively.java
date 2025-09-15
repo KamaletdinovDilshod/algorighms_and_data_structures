522b050b421d8d51ee305eb71e136ee0c54e034f
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-09-15 17:11:20 +0500

    Solution 2

--- src/leetcode/easy/MergeStringsAlternatively.java
+++ src/leetcode/easy/MergeStringsAlternatively.java
@@ -26,4 +26,22 @@ public class MergeStringsAlternatively {
     }
 
 
+    // Solution 3
+
+    public String mergeAlternately2(String word1, String word2) {
+        StringBuilder sb = new StringBuilder(word1.length() + word2.length());
+        int n = Math.max(word1.length(), word2.length());
+
+        for (int i = 0; i < n; i++) {
+            if (i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
