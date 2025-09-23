c8131ca6d0d6d522887b4535b4fe1088f3016e12
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-09-23 16:18:36 +0500

    ReverseWordsInAString

--- src/leetcode/easy/ReverseWordsInAString.java
+++ src/leetcode/easy/ReverseWordsInAString.java
@@ -32,37 +32,50 @@ public class ReverseWordsInAString {
         return sb.toString().trim();
     }
 
-    public static String reverseWords(String s) {
-        String[] words = s.split(" ");
-        int i = 0;
-        int j = words.length - 1;
+    public String reverseWords(String s) {
+        if (s == null) return null;
 
+        char[] a = s.toCharArray();
+        int n = a.length;
+
+        // step 1. reverse the whole string
+        reverse(a, 0, n - 1);
+        // step 2. reverse each word
+        reverseWords(a, n);
+        // step 3. clean up spaces
+        return cleanSpaces(a, n);
+    }
+
+    void reverseWords(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (i < n) {
+            while (i < j || i < n && a[i] == ' ') i++; // skip spaces
+            while (j < i || j < n && a[j] != ' ') j++; // skip non spaces
+            reverse(a, i, j - 1);                   // reverse the word
+        }
+    }
+
+    // trim leading, trailing and multiple spaces
+    String cleanSpaces(char[] a, int n) {
+        int i = 0, j = 0;
+
+        while (j < n) {
+            while (j < n && a[j] == ' ') j++;             // skip spaces
+            while (j < n && a[j] != ' ') a[i++] = a[j++]; // keep non spaces
+            while (j < n && a[j] == ' ') j++;             // skip spaces
+            if (j < n) a[i++] = ' ';                      // keep only one space
+        }
+
+        return new String(a).substring(0, i);
+    }
+
+    // reverse a[] from a[i] to a[j]
+    private void reverse(char[] a, int i, int j) {
         while (i < j) {
-            if ((!Objects.equals(words[i], " ") && !Objects.equals(words[j], " ") && !Objects.equals(words[j], "") && !Objects.equals(words[i], ""))) {
-                String temp = words[i].trim();
-                words[i] = words[j].trim();
-                words[j] = temp.trim();
-                i++;
-                j--;
-            } else if (Objects.equals(words[i], "") || Objects.equals(words[i], " ")) {
-                words[i] = null;
-                i++;
-            } else if (Objects.equals(words[j], "") || Objects.equals(words[j], " ")) {
-                words[j] = null;
-                j--;
-            }
+            char t = a[i];
+            a[i++] = a[j];
+            a[j--] = t;
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < words.length; k++) {
-            System.out.println("THIS KKK: " + words[k]);
-            if (!Objects.equals(words[k], " ") || !Objects.equals(words[k], "") || words[k] != null) {
-                if (k != 0) {
-                    sb.append("-");
-                }
-                sb.append(words[k]);
-            }
-        }
-        return sb.toString();
     }
 }
