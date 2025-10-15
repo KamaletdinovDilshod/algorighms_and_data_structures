1d14a43003fdb357a6f1b00d5c98ab19e88d8e84
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-10-15 13:07:25 +0500

    CountGoodNodesInBinaryTree

---
+++ src/leetcode/medium/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+
+public class CountGoodNodesInBinaryTree {
+
+    int count = 0;
+    int maxVal = Integer.MIN_VALUE;
+
+    void helper(TreeNode node) {
+        if (node == null) return;
+
+        int oldMax = maxVal;
+        maxVal = Math.max(maxVal, node.val);
+
+        if (node.val == maxVal) count++;
+
+        helper(node.left);
+        helper(node.right);
+
+        maxVal = oldMax;
+    }
+
+    public int goodNodes(TreeNode root) {
+        helper(root);
+        return count;
+    }
+
+}
