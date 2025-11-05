4e7b38a525d146cd5c45162d8e3bf7b0f3c87fd8
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-06 01:38:40 +0500

    ConvertSortedArrayToBinarySearchTree

---
+++ src/leetcode/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,23 @@
+package leetcode.easy;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return solve(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode solve(int[] nums, int start, int end) {
+        if (start > end) return null;
+
+        int mid = (start + end) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = solve(nums, start, mid - 1);
+        root.right = solve(nums, mid + 1, end);
+        return root;
+    }
+}
