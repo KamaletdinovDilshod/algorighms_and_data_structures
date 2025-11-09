092466cb7e5a57424d67329c748412953fbd4494
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-10 01:25:51 +0500

    InvertBinaryTree

---
+++ src/leetcode/easy/InvertBinaryTree.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class InvertBinaryTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+
+    public void helper(TreeNode root) {
+        if (root == null) return;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        helper(root.left);
+        helper(root.right);
+    }
+}
