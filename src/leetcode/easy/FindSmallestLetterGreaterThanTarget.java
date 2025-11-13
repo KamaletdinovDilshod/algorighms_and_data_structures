154b44666f988836279bb1cb6085d05d599a8147
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-13 23:00:10 +0500

    FindSmallestLetterGreaterThanTarget

---
+++ src/leetcode/easy/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+public class FindSmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] arr = {'c', 'f', 'j'};
+        System.out.println(nextGreatestLetter(arr, 'x'));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length - 1;
+        int mid;
+        int pos = -1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (letters[mid] > target) {
+                pos = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return pos == -1 ? letters[0] : letters[pos];
+
+    }
+}
