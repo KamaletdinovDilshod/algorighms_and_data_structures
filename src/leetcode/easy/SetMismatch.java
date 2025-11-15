462c1cd6a83b7ff06181723afc1532b353e94912
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-16 01:43:02 +0500

    SetMismatch

--- src/leetcode/easy/SetMismatch.java
+++ src/leetcode/easy/SetMismatch.java
@@ -1,6 +1,7 @@
 package leetcode.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SetMismatch {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class SetMismatch {
     }
 
     public static int[] findErrorNums(int[] nums) {
-        int dup = 0, lost , xor = 0;
+        int dup = 0, lost, xor = 0;
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             if (i + 1 < nums.length && nums[i] == nums[i + 1]) {
@@ -19,6 +20,26 @@ public class SetMismatch {
         }
         lost = (xor ^ dup);
         return new int[]{dup, lost};
+    }
+
+    // 2 Soulution
+
+    public static int[] findErrorNums2(int[] nums) {
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        int[] ans = {0, 0};
+
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+
+        for (int i = 1; i < nums.length + 1; i++) {
+            if (counter.getOrDefault(i, 0) == 0) {
+                ans[1] = i;
+            } else if (counter.getOrDefault(i, 0) == 2) {
+                ans[0] = i;
+            }
+        }
+        return ans;
     }
 }
 
