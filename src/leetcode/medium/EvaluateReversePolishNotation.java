ac9eddb99cd05cadecca7a6dd585d6344c32acaa
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-20 02:43:09 +0500

    EvaluateReversePolishNotation

---
+++ src/leetcode/medium/EvaluateReversePolishNotation.java
@@ -0,0 +1,35 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    public static void main(String[] args) {
+        String[] tokens = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        System.out.println(evalRPN(tokens));
+    }
+
+    public static int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                int result = switch (token) {
+                    case "+" -> a + b;
+                    case "-" -> a - b;
+                    case "*" -> a * b;
+                    case "/" -> a / b;
+                    default -> throw new IllegalArgumentException("Unsupported operator: " + token);
+                };
+
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
