5b9d54b60b36c3583590daaaf6076376e2d516ec
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-11-25 15:12:30 +0500

    MaximumSubsequenceScore

---
+++ src/leetcode/medium/SmallestNumberInInfiniteSet.java
@@ -0,0 +1,34 @@
+package leetcode.medium;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+class SmallestInfiniteSet {
+
+    private int current;
+    private PriorityQueue<Integer> minHeap;
+    private Set<Integer> addedBack;
+
+    public SmallestInfiniteSet() {
+        current = 1;
+        minHeap = new PriorityQueue<>();
+        addedBack = new HashSet<>();
+    }
+
+    public int popSmallest() {
+        if (!minHeap.isEmpty()) {
+            int smallest = minHeap.poll();
+            addedBack.remove(smallest);
+            return smallest;
+        }
+        return current++;
+    }
+
+    public void addBack(int num) {
+        if (num < current && !addedBack.contains(num)) {
+            minHeap.offer(num);
+            addedBack.add(num);
+        }
+    }
+}
