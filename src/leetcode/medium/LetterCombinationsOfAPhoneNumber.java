c815c9a730ac3aa830f491f2fdf9faa444158ece
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-12-08 19:47:07 +0500

    remove unused code

--- src/leetcode/medium/LetterCombinationsOfAPhoneNumber.java
+++ src/leetcode/medium/LetterCombinationsOfAPhoneNumber.java
@@ -12,8 +12,6 @@ public class LetterCombinationsOfAPhoneNumber {
         System.out.println(letterCombinations(digits));
     }
 
-    static String[] phoneCharacters = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
     public static List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
         if (digits.isEmpty()) return result;
