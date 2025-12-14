d9e8071eba2ea9ede115e9188d7b0bea895c4949
KamaletdinovDilshod <kamaletdinovdilshod01@gmail.com>
2025-12-15 02:10:25 +0500

    UniquePaths

--- src/leetcode/medium/UniquePaths.java
+++ src/leetcode/medium/UniquePaths.java
@@ -8,23 +8,18 @@ public class UniquePaths {
     }
 
     public static int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) return 1;
-        int[][] arrray = new int[m][n];
-        int sum = 0;
-        for (int j = 0; j < n; j++) {
-            arrray[0][j] = 1;
-        }
-        for (int i = 0; i < m; i++) {
-            arrray[i][0] = 1;
-        }
-        for (int k = 0; k < m - 1; k++) {
-            int q = k + 1;
-            for (int a = 1; a < n; a++) {
-                int l = a - 1;
-                sum = arrray[k][a] + arrray[q][l];
-                arrray[q][l + 1] = sum;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i == m - 1 && j == n - 1) {
+                    dp[m - 1][n - 1] = 1;
+                    continue;
+                }
+                dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
             }
         }
-        return sum;
+
+        return dp[0][0];
     }
 }
